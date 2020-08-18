6b6e6f58a5cf215b92b782b86022f3bad7510303
Trent Schneider <trentonschneider@gmail.com>
2020-08-17 21:24:42 -0400

    updated seed file

--- seed.sql
+++ seed.sql
@@ -3,14 +3,18 @@ VALUES ("Accounting");
 INSERT INTO departments(name)
 VALUES ("IT");
 INSERT INTO roles(title, salary, department_id)
-VALUES ("Manager", 75000, 1);
+VALUES ("Accounting Manager", 75000, 1);
 INSERT INTO employees(first_name, last_name, role_id)
 VALUE ("John", "Smith", 1);
 INSERT INTO roles(title, salary, department_id)
+VALUES ("IT Manager", 80000, 2);
+INSERT INTO employees(first_name, last_name, role_id)
+VALUES ("Charles", "Alexander", 2);
+INSERT INTO roles(title, salary, department_id)
 VALUES ("Supervisor", 45000, 1);
 INSERT INTO employees(first_name, last_name, role_id, manager_id)
-VALUES ("Tom", "David", 2, 1);
+VALUES ("Tom", "David", 3, 1);
 INSERT INTO roles(title, salary, department_id)
 VALUES ("IT Intern", 15000, 2);
 INSERT INTO employees(first_name, last_name, role_id, manager_id)
-VALUES ("Chris", "Jones", 3, 1);
+VALUES ("Chris", "Jones", 4, 2);
